100f2fdbebedca5f4797e752b4a43458ebd063f1
dikarizki55 <dikaesmidorikawa55@gmail.com>
2024-01-21 16:44:19 +0700

    work well, just need little bit adjustment

--- index.php
+++ index.php
@@ -82,54 +82,101 @@ function getStartSeconds($datacsv, $video)
     return floatval($timestampvideo - $timestampcsv);
 };
 
+function sliceScandir(string $inputname)
+{
+    return array_slice(scandir($_POST[$inputname]), 2, count(scandir($_POST[$inputname])) - 2);
+}
+
+function arrayfilepath($arrayname)
+{
+    $array = sliceScandir($arrayname);
+    $newarray = array();
+    foreach ($array as $item) {
+        array_push($newarray, $_POST[$arrayname] . "\\" . $item);
+    };
+    return $newarray;
+}
+
+function csvSelector($array, $filemtime, $i)
+{
+    $videomtime = floatval($filemtime);
+
+    $csvfile = readCsv($array[$i]);
+    $csvstart = floatval($csvfile[1][0] / 1000000000);
+    $csvend = floatval($csvfile[count($csvfile) - 2][0] / 1000000000);
+
+    if ($csvstart < $videomtime and $videomtime < $csvend) {
+        return readCsv($array[$i]);
+    } else {
+        if (array_key_exists($i + 1, $array)) {
+            csvSelector($array, $videomtime, $i + 1);
+        } else {
+            var_dump("file csv tidak tersedia untuk $filemtime ini");
+        }
+    }
+}
+
 if (isset($_POST['download1'])) {
 
-    if (!empty($_FILES["csv"]["tmp_name"]) and !empty($_FILES['video']["tmp_name"])) {
-        echo "<pre>";
-        print_r($_FILES);
-        echo "</pre>";
+    // if (!empty($_FILES["csv"]["tmp_name"]) and !empty($_FILES['video']["tmp_name"])) {
+    // echo "<pre>";
+    // print_r($_FILES);
+    // echo "</pre>";
 
-        $csvarray = readCsv($_FILES["csv"]["tmp_name"]);
+    // $csvarray = readCsv($_FILES["csv"]["tmp_name"]);
 
-        $listcsv = array();
-        foreach ($_FILES['video']["name"] as $name) {
-            array_push($listcsv, substr($name, 0, strlen($name) - 4));
-        };
+    $listcsv = array();
+    foreach (sliceScandir('video') as $name) {
+        array_push($listcsv, substr($name, 0, strlen($name) - 4));
+    };
 
-        echo "<pre>";
-        // print_r(makeData($csvarray, 'testfile/00383.Mts', $ffprobe));
-        // print_r(searchKey($csvarray, 1, 2));
-        echo getStartSeconds($csvarray, $_FILES['video']["tmp_name"][0]);
-        echo "</pre>";
+    // $modvideo = filemtime($_POST['video'] . sliceScandir('video')[0]);
 
-        // $archiveFileName = 'baru.zip';
-        // $zip = new ZipArchive;
-        // if ($zip->open($archiveFileName, ZipArchive::CREATE) === TRUE) {
-        //     // Add files to the zip file
-
-        //     $i = 0;
-        //     foreach ($listcsv as $name) {
-        //         $zip->addFile(makeFileCsv(makeData($csvarray, $_FILES['video']["tmp_name"][$i], $ffprobe), $name));
-        //         $i++;
-        //     }
+    echo "<pre>";
 
 
-        //     // All files are added, so close the zip file.
-        //     $zip->close();
-        // }
 
-        // header("Content-type: application/zip");
-        // header("Content-Disposition: attachment; filename=$archiveFileName");
-        // header("Content-length: " . filesize($archiveFileName));
-        // header("Pragma: no-cache");
-        // header("Expires: 0");
-        // readfile("$archiveFileName");
+    echo "<br>";
+    echo "<br>";
+    // print(csvSelector(arrayfilepath('csv'), filemtime(arrayfilepath('video')[0]), 0));
+    echo "<br>";
+    echo "<br>";
 
-        // unlink($archiveFileName);
-        // foreach ($listcsv as $filecsv) {
-        //     unlink("$filecsv.csv");
-        // }
-    }
+
+    print_r(makeData(csvSelector(arrayfilepath('csv'), filemtime(arrayfilepath('video')[0]), 0), arrayfilepath('video')[0], $ffprobe));
+
+    // print_r(searchKey($csvarray, 1, 2));
+    // echo getStartSeconds($csvarray, $_FILES['video']["tmp_name"][0]);
+    echo "</pre>";
+
+    // $archiveFileName = 'baru.zip';
+    // $zip = new ZipArchive;
+    // if ($zip->open($archiveFileName, ZipArchive::CREATE) === TRUE) {
+    //     // Add files to the zip file
+
+    //     $i = 0;
+    //     foreach ($listcsv as $name) {
+    //         $zip->addFile(makeFileCsv(makeData($csvarray, $_FILES['video']["tmp_name"][$i], $ffprobe), $name));
+    //         $i++;
+    //     }
+
+
+    //     // All files are added, so close the zip file.
+    //     $zip->close();
+    // }
+
+    // header("Content-type: application/zip");
+    // header("Content-Disposition: attachment; filename=$archiveFileName");
+    // header("Content-length: " . filesize($archiveFileName));
+    // header("Pragma: no-cache");
+    // header("Expires: 0");
+    // readfile("$archiveFileName");
+
+    // unlink($archiveFileName);
+    // foreach ($listcsv as $filecsv) {
+    //     unlink("$filecsv.csv");
+    // }
+    // }
 }
 
 
@@ -173,29 +220,13 @@ if (isset($_POST['download1'])) {
     //     echo getVideoDuration($video, $ffprobe);
     // }
 
-    $listcsv = array('00383', '00384', '00385', '00386');
-    $i = 0;
-    foreach ($listcsv as $csv) {
-        echo "<br>";
-        echo $csv;
-        echo "<br>";
-        echo $i;
-        $i++;
-    };
-
-    $string = '00383.Mts';
-    echo substr($string, 0, strlen($string) - 4);
-
-    echo "<br>";
-
-
     // print_r(makeData($csvarray, $listvideo[2], $ffprobe));
 
-    var_dump('C:\xampp\htdocs\gyroflowcsvdivider\testfile');
-    var_dump(scandir('D:\testfile\\'));
-    var_dump(scandir('../ffmpeg/bin/'));
+    // var_dump('C:\xampp\htdocs\gyroflowcsvdivider\testfile');
+    // var_dump(scandir('D:\testfile'));
+    // var_dump(scandir('../ffmpeg/bin/'));
 
-    var_dump(getVideoDuration('D:\testfile\00383.Mts', $ffprobe));
+    // var_dump(getVideoDuration('D:\testfile\00383.Mts', $ffprobe));
 
     // tanda file terakhir
     ?>
@@ -204,7 +235,11 @@ if (isset($_POST['download1'])) {
 
 
 <form method="post" enctype="multipart/form-data">>
-    <input type="file" name="csv" id="csv">
-    <input type="file" name="video[]" id="video" multiple>
+    <label for="csv">Input Csv Folder</label>
+    <input type="text" id="csv" name="csv" value="D:\testfile\csv">
+    <br>
+    <label for="csv">Input Video Folder</label>
+    <input type="text" id="video" name="video" value="D:\testfile\video">
+    <br>
     <input type="submit" name="download1" value="download" />
 </form>
